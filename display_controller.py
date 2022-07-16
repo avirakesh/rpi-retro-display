fd86bd03658b825e22d9f14c1e4bb668467ffa2f
Avichal Rakesh <avichal.rakesh@gmail.com>
2022-07-16 00:51:06 -0700

    add license to source files

--- display_controller.py
+++ display_controller.py
@@ -1,3 +1,10 @@
+###############################################################################
+# "THE BEER-WARE LICENSE" (Revision 42):
+# Avichal Rakesh wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return. Avichal Rakesh
+###############################################################################
+
 from dataclasses import dataclass
 from PIL import Image
 import time
