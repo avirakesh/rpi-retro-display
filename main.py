2d70dfb389929959a5c1249b4d855a8863d0bfad
Avichal Rakesh <avichal.rakesh@gmail.com>
2022-07-19 21:55:51 -0700

    add basic time based automation, and fix bug in main thread causing it to spin instead of sleep

--- main.py
+++ main.py
@@ -6,34 +6,133 @@
 ###############################################################################
 
 import time
+
+from numpy import exp
 from pixlet_wrapper import PixletWrapper
 from display_controller import DisplayControllerDelegator
 
+_SECS_IN_AN_HOUR = 60 * 60
+_SECS_IN_A_DAY = 24 * _SECS_IN_AN_HOUR
 _MS_TO_S = 0.001
 JSON_PATH = "config.json"
 
 def main():
-    with DisplayControllerDelegator() as display_controller, PixletWrapper(JSON_PATH) as pixlet_wrapper:
-        applet = pixlet_wrapper.get_current_applet()
-        print("Displaying Applet", applet["name"])
+    with DisplayControllerDelegator() as display_controller, \
+         PixletWrapper(JSON_PATH) as pixlet_wrapper:
 
-        gif_path = pixlet_wrapper.create_gif_from_sketch(applet["path"])
-        display_controller.queue_gif_to_display(gif_path)
+        # start by forcing a render of the applet
+        (curr_applet, next_applet_time) = pixlet_wrapper.get_current_applet()
+        print(f"Displaying Applet: {curr_applet['name']}")
+        curr_render_time = _render_applet_if_needed(pixlet_wrapper, display_controller, curr_applet)
 
+        # main program loop
         try:
             while True:
-                if not applet["dynamic"]:
-                    time.sleep(60) # refresh every 60 seconds for non-dynamic display
-                    continue
+                if _should_update_applet(curr_applet, next_applet_time):
+                    (curr_applet, next_applet_time) = pixlet_wrapper.get_current_applet()
+                    # Force render the new applet
+                    print(f"Displaying Applet: {curr_applet['name']}")
+                    curr_render_time = _render_applet_if_needed(pixlet_wrapper,
+                                                                display_controller,
+                                                                curr_applet)
+                elif curr_applet["dynamic"]:
+                    curr_render_time = _render_applet_if_needed(pixlet_wrapper,
+                                                                display_controller,
+                                                                curr_applet,
+                                                                curr_render_time)
 
-                gif_path = pixlet_wrapper.create_gif_from_sketch(applet["path"])
-                display_controller.queue_gif_to_display(gif_path)
-
-                update_time = time.perf_counter() + applet["refresh_interval_ms"]
-                while time.perf_counter() > update_time:
-                    time.sleep(update_time - time.perf_counter())
+                wakeup_time = _get_wake_up_time(curr_applet, curr_render_time, next_applet_time)
+                while time.perf_counter() < wakeup_time:
+                    time.sleep(wakeup_time - time.perf_counter())
         except KeyboardInterrupt:
             pass
+
+
+# returns True if the thread should ask PixletWrapper for a new applet, False otherwise
+def _should_update_applet(curr_applet, next_applet_time):
+    if next_applet_time is None:
+        # Never update applet
+        return False
+
+    curr_applet_time = curr_applet["start_time"]
+    curr_time = PixletWrapper.get_day_time_secs()
+
+    # Simple case: next applet is scheduled for sometime today
+    if curr_applet_time < next_applet_time :
+         return curr_time >= next_applet_time
+
+    # Weird case: Last applet of the day is currently displayed
+
+    # We're still in the same day
+    # Do not update applet as this is the last applet of the day
+    if curr_time >= curr_applet_time:
+        return False
+
+    # It is the next day!
+    # Update applet if curr_time is past next_applet_time
+    return curr_time >= next_applet_time
+
+
+# Returns the time at which this thread should wake up. This could be to update the applet, to
+# re-render the applet, or just to keep the OS from deprioritizing the script.
+# This is calculated as minimum of time for curr_applet to update and time at which the current
+# applet expires.
+def _get_wake_up_time(curr_applet, curr_applet_render_time, next_applet_day_time):
+    curr_time = time.perf_counter()
+    curr_day_time = PixletWrapper.get_day_time_secs()
+
+    # figure out time after which the applet should be updated
+    time_to_next_applet = 0
+    if next_applet_day_time is None:
+        # No next applet, default to an hour
+        time_to_next_applet = _SECS_IN_AN_HOUR
+    if curr_day_time > next_applet_day_time:
+        # next_applet_time is the next day
+        # Wait all of today + until next applet has to come up
+        time_to_next_applet = (_SECS_IN_A_DAY - curr_day_time) + next_applet_day_time
+    else:
+        # next_applet_time is on the same day
+        time_to_next_applet = next_applet_day_time - curr_day_time
+
+    next_app_time = curr_time + time_to_next_applet
+
+
+
+    time_to_update_curr = 0
+    if not curr_applet["dynamic"]:
+        # Static applet, default to an hour
+        time_to_curr_applet = _SECS_IN_AN_HOUR
+    else:
+        curr_applet_expiry = curr_applet_render_time + (curr_applet["refresh_interval_ms"] * _MS_TO_S)
+        time_to_curr_applet = curr_applet_expiry - curr_time
+
+    return curr_time + min(time_to_next_applet, time_to_curr_applet, _SECS_IN_AN_HOUR)
+
+
+# Queues passed applet to display_controller
+# current_render_time = None forces the applet to be queued
+# return the time at which the applet was queued to render
+def _render_applet_if_needed(pixlet_wrapper, display_controller, applet, curr_render_time=None):
+    if curr_render_time is None:
+        # Force expired
+        expired = True
+    else:
+        expiry = curr_render_time + (applet["refresh_interval_ms"] * _MS_TO_S)
+        expired = time.perf_counter() >= expiry
+
+    if not expired:
+        # return early if the applet has not expired yet
+        return curr_render_time
+
+    gif_path = pixlet_wrapper.create_gif_from_sketch(applet["path"])
+    if gif_path is not None:
+        display_controller.queue_gif_to_display(gif_path)
+    else:
+        print(f"Error creating gif for '{applet['name']}'")
+        # didn't render, don't update render time
+        return curr_render_time
+
+    return time.perf_counter()
 
 if __name__ == "__main__":
     main()
--- pixlet_wrapper.py
+++ pixlet_wrapper.py
@@ -7,8 +7,12 @@
 
 import subprocess
 import json
+import re
+import time
+from sortedcontainers import SortedDict
 
 _OUTPUT_DIR = "gifs"
+_TIME_REGEX = r"(\d\d):(\d\d)" # pattern for hh:mm
 
 
 class PixletWrapper:
@@ -25,12 +29,15 @@ class PixletWrapper:
             print("Failed to create output 'gifs' directory")
             exit(1)
 
+
     def __enter__(self):
         with open(self._json_path) as json_file:
             json_data = json.load(json_file)
-            self._applets = json_data["applets"]
+            self._init_applets(json_data["applets"])
+            self._raw_applets = json_data["applets"]
 
         return self
+
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         rm = subprocess.run(["rm", "-rf", _OUTPUT_DIR])
@@ -38,6 +45,7 @@ class PixletWrapper:
             print("Failed to remove", _OUTPUT_DIR)
         else:
             print("Deleted dir:", _OUTPUT_DIR)
+
 
     def create_gif_from_sketch(self, applet_path):
         applet_name = applet_path.split("/")[-1]
@@ -58,7 +66,97 @@ class PixletWrapper:
 
         return output_path
 
+
+    # returns (current_applet, next_applet_time)
+    # returns (current_applet, None) if there is only one applet
+    #
+    # Called by main.py at the start and then again when get_day_time_secs() returns a
+    # value >= the returned next_applet_time. For simplicity, this function doesn't make assumtions
+    # based on previous calls.
     def get_current_applet(self):
-        # TODO: Add applet automation through config
-        # Just assume the first config is the active one for now.
-        return self._applets[0]
+        # Simple time base automation.
+        # Can be updated for more complex automation.
+
+        if len(self._applets) == 1:
+            return (self._applets.peekitem(0)[1], None)
+
+        curr_time = PixletWrapper.get_day_time_secs()
+        curr_applet_idx = self._get_current_applet_idx(curr_time)
+        next_applet_idx = (curr_applet_idx + 1) % len(self._applets)
+
+        curr_applet = self._applets.peekitem(curr_applet_idx)[1]
+        next_applet_time = self._applets.peekitem(next_applet_idx)[0]
+
+
+        return (curr_applet, next_applet_time)
+
+
+    def _get_current_applet_idx(self, curr_time):
+        curr_idx = self._applets.bisect_key_left(curr_time)
+
+        if curr_idx == len(self._applets):
+            return -1
+
+
+        if self._applets.peekitem(curr_idx)[0] == curr_time:
+            return curr_idx
+
+        return curr_idx - 1
+
+
+    def _init_applets(self, applets):
+        start_time_to_applet = {}
+        for applet in applets:
+            start_time = applet["start_time"]
+
+            if start_time_to_applet.get(start_time) is not None:
+                print("Error: Found two applets with the same start time.")
+                print("Start Time:", start_time)
+                print("Applets:", start_time_to_applet[start_time]["name"], "and", applet["name"])
+                exit(1)
+
+            start_time_to_applet[start_time] = applet
+
+        self._setup_applets(start_time_to_applet)
+
+
+    def _setup_applets(self, start_time_to_applet):
+        self._applets = SortedDict(PixletWrapper.identity_fn)
+
+        for start_time_str, applet in start_time_to_applet.items():
+            start_time = PixletWrapper._parse_and_assert_time(start_time_str)
+            applet["start_time"] = start_time
+            self._applets[start_time] = applet
+
+
+    @staticmethod
+    def _parse_and_assert_time(time_str):
+        match = re.findall(_TIME_REGEX, time_str)
+        assert len(match) == 1, \
+               f"Invalid time: '{time_str}'. start_time should match the pattern hh:mm"
+
+        hhmm = match[0]
+        (hh, mm) = (int(hhmm[0]), int(hhmm[1]))
+        assert 0 <= hh and 23 >= hh, \
+               f"Invalid time: '{time_str}'. {hh} should be between 0 and 23 inclusive"
+        assert 0 <= mm and 59 >= mm, \
+               f"Invalid time: '{time_str}'. {mm} should be between 0 and 59 inclusive"
+
+        return (60 * 60 * hh) + (mm * 60)
+
+
+
+    # returns the time passed in seconds since 00:00:00 hrs (midnight)
+    # Time resolution is of seconds
+    # output range: [0, 86340)
+    @staticmethod
+    def get_day_time_secs():
+        hh = int(time.strftime("%H"))
+        mm = int(time.strftime("%M"))
+        ss = int(time.strftime("%S"))
+        return (hh * 60 * 60) + (mm * 60) + ss
+
+
+    @staticmethod
+    def identity_fn(x):
+        return x
