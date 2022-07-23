7342c1660372cb7ced5ebdcc63893611b2e0b15e
Avichal Rakesh <avichal.rakesh@gmail.com>
2022-07-23 01:25:54 -0700

    add a simple display test script

--- play.py
+++ play.py
@@ -7,26 +7,58 @@
 
 import time
 import sys
-
+from getch import getche
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 
 ## Simple script to check if the diplay is working or not.
 
+ROWS = 32
+COLS = 64
+
 # Configuration for the matrix
 options = RGBMatrixOptions()
-options.rows = 32
-options.cols = 64
+options.rows = ROWS
+options.cols = COLS
 options.chain_length = 1
 options.parallel = 1
 options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
 
 matrix = RGBMatrix(options = options)
 
-matrix.Fill(255, 0, 0)
+print("Press x to stop.")
 
-try:
-    print("Press CTRL-C to stop.")
-    while True:
-        time.sleep(100)
-except KeyboardInterrupt:
-    sys.exit(0)
+prev_pos = [0, 0]
+pos = [0, 0]
+
+color = [255, 255, 255]
+
+while True:
+    input = getche()
+    print()
+    if input == "x":
+        break
+
+    if input == "w":
+        pos[0] = (pos[0] - 1) % ROWS
+    elif input == "s":
+        pos[0] = (pos[0] + 1) % ROWS
+    elif input == "a":
+        pos[1] = (pos[1] - 1) % COLS
+    elif input == "d":
+        pos[1] = (pos[1] + 1) % COLS
+    elif input == "r":
+        color[0] = 255 if color[0] == 0 else 0
+    elif input == "g":
+        color[1] = 255 if color[1] == 0 else 0
+    elif input == "b":
+        color[2] = 255 if color[2] == 0 else 0
+    elif input == "i":
+        color = [255, 255, 255]
+    elif input == "c":
+        matrix.Fill(0, 0, 0)
+        pos = [0, 0]
+        color = [0, 0, 0]
+
+    matrix.SetPixel(pos[1], pos[0], color[0], color[1], color[2])
+
+    prev_pos = pos
